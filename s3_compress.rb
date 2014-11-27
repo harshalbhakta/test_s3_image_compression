3920f42731caebb8e5e6abd4f2d9bfdb3b6b8828
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2014-11-28 01:37:46 +0530

    Initial commit

---
+++ s3_compress.rb
@@ -0,0 +1,34 @@
+require 'aws-sdk'
+require 'RMagick'
+require 'pathname'
+
+s3 = AWS::S3.new :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_KEY"]
+bucket = s3.buckets['learnapt-dev'] # no request made
+s3_image_path = Pathname.new("22.png") 
+obj = bucket.objects[s3_image_path.to_s] # makes no request, returns an AWS::S3::S3Object
+
+image_path = File.expand_path("./output/" + s3_image_path.basename.to_s)
+thumb_path = File.expand_path("./output/thumb_" + s3_image_path.basename.to_s)
+
+# streaming download from S3 to a file on disk
+File.open(image_path, 'w+') do |file|
+  obj.read do |chunk|
+     file.write(chunk)
+  end
+end
+
+# Read first image from file
+image = Magick::Image::read(image_path).first
+
+image.format = 'JPEG'
+
+image.write(image_path.gsub("png","jpg")) # { self.quality = ARGV[0].to_i }
+
+# Resize image to maxium dimensions
+image.resize_to_fit!(250)
+
+# Write image to file system
+image.write(thumb_path.gsub("png","jpg")) # { self.quality = ARGV[0].to_i }
+
+# Free image from memory
+image.destroy!
