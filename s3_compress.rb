a0b33c323ea342c4803be7eb1bf84fec9022c82a
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2014-11-28 01:41:44 +0530

    Add command line args

--- s3_compress.rb
+++ s3_compress.rb
@@ -4,7 +4,7 @@ require 'pathname'
 
 s3 = AWS::S3.new :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_KEY"]
 bucket = s3.buckets['learnapt-dev'] # no request made
-s3_image_path = Pathname.new("22.png") 
+s3_image_path = Pathname.new(ARGV[0].to_s) 
 obj = bucket.objects[s3_image_path.to_s] # makes no request, returns an AWS::S3::S3Object
 
 image_path = File.expand_path("./output/" + s3_image_path.basename.to_s)
@@ -22,13 +22,13 @@ image = Magick::Image::read(image_path).first
 
 image.format = 'JPEG'
 
-image.write(image_path.gsub("png","jpg")) # { self.quality = ARGV[0].to_i }
+image.write(image_path.gsub("png","jpg")) # { self.quality = ARGV[1].to_i }
 
 # Resize image to maxium dimensions
 image.resize_to_fit!(250)
 
 # Write image to file system
-image.write(thumb_path.gsub("png","jpg")) # { self.quality = ARGV[0].to_i }
+image.write(thumb_path.gsub("png","jpg")) # { self.quality = ARGV[1].to_i }
 
 # Free image from memory
 image.destroy!
